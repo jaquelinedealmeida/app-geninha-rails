c6b46e6de1bca80b256a1196b2b6a828631869ca
Jaqueline de Almeida <jaquelinedealmeida@MacBook-Pro-de-Jaqueline.local>
2022-10-24 17:54:22 -0300

    Inclusao de id na rota, view e controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,10 @@ class SessionsController < ApplicationController
   def index
     @sessions = Session.all
   end
+  
+  def show
+    @session = Session.find(params[:id])
+
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "sessions#index"
 
   get "/sessions",  to: "sessions#index"
+  get "/sessions/:id", to: "sessions#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
