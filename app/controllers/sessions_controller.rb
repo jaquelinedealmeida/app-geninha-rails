d6fa0a201152e78b461cdf1cbae03182763c9ffe
Jaqueline de Almeida <jaquelinedealmeida@MacBook-Pro-de-Jaqueline.local>
2022-10-25 15:46:58 -0300

    Inclusao de validacoes, metodo edit e update e partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,21 +7,36 @@ class SessionsController < ApplicationController
     @session = Session.find(params[:id])
   end
 
-  def show
-    @session = Session.find(params[:id])
-  end
-
   def new 
-    @article = Session.new
+    @session = Session.new
   end 
 
   def create
-    @article = Session.new(session_params)
+    @session = Session.new(session_params)
 
     if @session.save
-      redirection_to @session
+      redirect_to @session
     else
       render :new, status: :unprocessable_entity
     end 
   end
+
+  def edit 
+    @session = Session.fin(params[:id])
+  end 
+
+  def update 
+    @session = Session.find(params[id])
+
+    if @session.update(session_params)
+      redirect_to @session
+    else 
+      render :edit, status: :unprocessable_entity
+    end
+  end   
+
+  private 
+    def session_params 
+      params.require(:article).permit(:title, :timetable)
+    end 
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,4 @@
 class Session < ApplicationRecord
+    validates :title, presence: true
+    validates :timetable, presence: true, length: { minimum: 10}
 end
