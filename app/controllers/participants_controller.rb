d49dc075f7e63fbb3e864427ff4ce0aa53caa3b2
Jaqueline de Almeida <jaquelinedealmeida@MacBook-Pro-de-Jaqueline.local>
2022-10-27 10:09:40 -0300

    Adicao da partial dos participants

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,3 +1,13 @@
 class ParticipantsController < ApplicationController
     def create 
+        @session = Session.find(params[:session_id])
+        @participant = @session.participants.create(participant_params)
+        redirect_to session_path(@session)
+    end
+
+    private
+    def participant_params
+        params.require(:participant).permit(:participants,
+    :role)
+    end
 end
